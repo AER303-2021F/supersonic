410df74e2258f019c0e708a9f8babe5f8a047a5d
Jai Willems <jai52h@hotmail.com>
2021-12-03 20:54:37 -0500

    MAIN: Add subsonic code.

--- code/analysis/subsonic_experimental.m
+++ code/analysis/subsonic_experimental.m
@@ -17,5 +17,15 @@ function mach = subsonic_experimental(pressure, gamma)
         The Mach numbers for the input pressure data.
     %}
     
-    mach = zeros(1,7);
+    % Prepare data.
+    static_pressure = pressure(1, :);
+    tot_pressure = pressure(2, :);
+    
+    % Prepare equation terms.
+    term_1 = 2 / (gamma - 1);
+    term_2 = (gamma - 1) / gamma;
+    
+    % Calculate Mach.
+    mach_squared = term_1 .* ((tot_pressure ./ static_pressure) .^ term_2 - 1);
+    mach = sqrt(mach_squared);
 end
